38c15b3f4fe283672389c57d96900d4f217ff736
ukn0wdawae <manvith10dayam@gmail.com>
2024-06-29 11:41:51 -0400

    getlib react query

--- app/library/page.tsx
+++ app/library/page.tsx
@@ -15,43 +15,76 @@ const LibraryCards = dynamic(() => import('@/components/LibraryCards'), {
 })
 
 
-
-
-export default function Page() {;
-  
-  const { user } = useUser();
-  const router  = useRouter()
-  const [predictions, setPredictions] = useState([])
-  async function GetLibrary(userId:string) {
-    const refreshedCompany = await fetch("/api/get-library", {
-      method: "POST",
-      body: JSON.stringify({userId: userId}),
-    }).then((res) =>  res.json());
-    return refreshedCompany
+// fetchFunctions.ts
+export const getLibrary = async (userId: string) => {
+  const response = await fetch("/api/get-library", {
+    method: "POST",
+    body: JSON.stringify({ userId: userId }),
+  });
+  if (!response.ok) {
+    throw new Error("Network response was not ok");
   }
-  useEffect(() => {
-    if(user){
-  const data  = GetLibrary(user?.id).then(
-    (data)  => setPredictions(data)
-  )
-  
-  console.log(data)
-    }
-  },[])
+  return response.json();
+};
+
+export default function Page() {
+  const { user } = useUser();
+  const router = useRouter();
+
+  // const [predictions, setPredictions] = useState([])
+
+  // async function GetLibrary(userId:string) {
+  //   const refreshedCompany = await fetch("/api/get-library", {
+  //     method: "POST",
+  //     body: JSON.stringify({userId: userId}),
+  //   }).then((res) =>  res.json());
+  //   return refreshedCompany
+  // }
+
+  // useEffect(() => {
+  //   if(user){
+  // const data  = GetLibrary(user?.id).then(
+  //   (data)  => setPredictions(data)
+  // )
+
+  // console.log(data)
+  //   }
+  // },[])
+
+  // Use React Query to fetch the data
+  const {
+    data: predictions,
+    error,
+    isLoading,
+  } = useQuery(["library", user?.id], () => getLibrary(user.id), {
+    enabled: !!user?.id, // Only run the query if the user is logged in
+  });
+
+  if (isLoading) {
+    return <Spinner animation="border" />;
+  }
+
+  if (error) {
+    return <div>Error loading data</div>;
+  }
 
   
-  
-  
-return(
-<>
-<h1 className='text-xl font-bold m-5'>Library</h1>
-<div className='flex border-t pt-10 flex-wrap gap-5'>
-{/* {!predictions && <Skeleton className="h-[200px] w-[300px] rounded-xl" /> } */}
-{predictions && predictions.map(item => (
-<LibraryCards key={item.output} targetVideo={item.input.target_video} swapImage={item.input.swap_image} output={item.outputUrl} />
-))}
-</div>
-{/* <div><p>{item.input.target_video}</p><p>{item.input.swap_image}</p><p>{item.outputUrl}</p></div> */}
-</>
-)
+  return (
+    <>
+      <h1 className="text-xl font-bold m-5">Library</h1>
+      <div className="flex border-t pt-10 flex-wrap gap-5">
+        {/* {!predictions && <Skeleton className="h-[200px] w-[300px] rounded-xl" /> } */}
+        {predictions &&
+          predictions.map((item) => (
+            <LibraryCards
+              key={item.output}
+              targetVideo={item.input.target_video}
+              swapImage={item.input.swap_image}
+              output={item.outputUrl}
+            />
+          ))}
+      </div>
+      {/* <div><p>{item.input.target_video}</p><p>{item.input.swap_image}</p><p>{item.outputUrl}</p></div> */}
+    </>
+  );
 }
